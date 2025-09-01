13d8f4378cf83c81b774079a96f1f064c0e3a5c0
balajouve <balakarunanithi.s@sixredmarbles.in>
2025-09-01 19:45:46 +0530

    Update site

--- v3insurance/settings.py
+++ v3insurance/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9ca94b7164ede9e30a54372ed0ec53e5'
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['v3insurance.onrender.com']
 INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','core']
 MIDDLEWARE = ['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware',]
 ROOT_URLCONF = 'v3insurance.urls'
